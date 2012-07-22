f93fe2f0c84bb6d29d255498d39791bd512981c8
Corona-Ayala <mcorona-ayala@MTVL10f1f8a62.local>
2012-07-21 19:13:36 -0700

    Added templates for the youtube api search request.

--- youSearcher.py
+++ youSearcher.py
@@ -3,11 +3,16 @@
 #
 # 2012-07-21
 ###############################################
+import urllib2
 
 class youtube(object):
 	def __init__(self):
 		pass
 
-	def search(self, track):
-		pass
-
+	def search(self,artist, track):
+         YTSERVER = "https://gdata.youtube.com/feeds/api/videos?"
+         Search = YTSERVER + "?%s+%s" % (artist, track)
+         print Search
+         SearchReq = urllib2.Request(url=YTSERVER,data=Search)
+         results = urllib2.urlopen(SearchReq)
+         print results.read()
